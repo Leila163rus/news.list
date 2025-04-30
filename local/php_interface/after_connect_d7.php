617da96493baaca94729a70647ce05294b529a6e
Пользователь <lilu163@mail.ru>
2025-04-30 10:36:14 +0400

    migration

---
+++ local/php_interface/after_connect_d7.php
@@ -0,0 +1,5 @@
+<?php
+$this->queryExecute("SET sql_mode=''");
+$this->queryExecute("SET NAMES 'utf8mb4'");
+$this->queryExecute("SET collation_connection = 'utf8mb4_0900_ai_ci'");
+$this->queryExecute("SET innodb_strict_mode=0");
